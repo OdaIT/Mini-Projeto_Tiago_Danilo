0d614ff9bcc04fdfeda9ef3da1819ab350c4ce4b
Tiago Machado <timac1997@gmail.com>
2025-12-04 11:38:53 +0000

    versao 1

--- TechStore.js
+++ TechStore.js
@@ -2,30 +2,59 @@
 
 //Opção 1: TechStore (Gestão de Inventário)
 
-class Produto{
+class Produto {
+
     constructor(nome, preco, quantidade, categoria){
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
         this.categoria = categoria;
-    }
-    atualizarPreço(){}
-    registarVenda(){}
-    reporStock(){}
-    devolucao(){}
-    quebras(){}
-    desconto(){}
-}
-const portatil = new Produto()
+    };
 
-class Inventario{
+    atualizarPreço(novoPreco) {
+        this.preco = novoPreco;        
+    };
+
+    registarVenda(unidades) {
+        this.quantidade -= unidades;       
+    };
+
+    reporStock(unidades) {
+        this.quantidade += unidades;
+    };
+
+    devolucao(unidades) {
+        this.quantidade += unidades;
+    };
+
+    quebras(unidades) {
+        this.quantidade -= unidades;
+    };
+
+    desconto(valor) {
+        if (valor > 0 && valor < 1) {
+            this.preco *= 1 - valor;
+        } else {
+            console.error("Valor inválido! Precisa ser decimal entre 0 e 1!")
+        }       
+    };
+
+}
+const portatil = new Produto("Portátil", 1000, 3, "Computadores");
+const desktop = new Produto("Desktop", 700, 2, "Computadores");
+const rato = new Produto("Rato", 15, 10, "Periféricos");
+const monitor = new Produto("Monitor", 150, 5, "Periféricos");
+const teclado = new Produto("Teclado", 20, 10, "Periféricos");
+
+class Inventario {
     constructor(array){
         this.produtos = array;
         this.historico = [];
     }
-    valorIventario(){}
+    valorInventario(){}
     premium(){}
     limpezaStock(){}
     filtroCategoria(){}
 }
 
+const totalProdutos = new Inventario([portatil, desktop, rato, monitor, teclado]);
